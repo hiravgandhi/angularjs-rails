cbb1e72a1adbc70a0e77f0d235c917e9ae65c611
Ben Walding <ben@walding.com>
2014-02-12 13:50:24 +1000

    javascripts directory is plural

--- lib/angularjs-rails/sprockets.rb
+++ lib/angularjs-rails/sprockets.rb
@@ -1,3 +1,3 @@
 require 'sprockets'
 
-Sprockets.append_path File.expand_path("../../../vendor/assets/javascript", __FILE__)
+Sprockets.append_path File.expand_path("../../../vendor/assets/javascripts", __FILE__)
