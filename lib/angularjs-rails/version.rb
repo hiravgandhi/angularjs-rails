230fa134874263c2805b654109f9cad360a3ca82
Hirav Gandhi <hgandhi@alumni.stanford.edu>
2015-10-01 09:28:59 -0700

    Bump version to 1.4.7

--- lib/angularjs-rails/version.rb
+++ lib/angularjs-rails/version.rb
@@ -1,6 +1,6 @@
 module AngularJS
   module Rails
-    VERSION = "1.4.4"
-    UNSTABLE_VERSION = "2.0.0-alpha.34"
+    VERSION = "1.4.7"
+    UNSTABLE_VERSION = "2.0.0-alpha.37"
   end
 end
--- vendor/assets/javascripts/angular-loader.js
+++ vendor/assets/javascripts/angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.4
+ * @license AngularJS v1.4.7
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -59,7 +59,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.4/' +
+    message += '\nhttp://errors.angularjs.org/1.4.7/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
